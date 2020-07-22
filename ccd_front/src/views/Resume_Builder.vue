042b5c50fbc72694477057a5a8f3fc780016271f
JulieZhang0102 <jzhang10@babson.edu>
2020-07-21 22:37:11 -0400

    update drag and resize

--- ccd_front/src/views/Resume_Builder.vue
+++ ccd_front/src/views/Resume_Builder.vue
@@ -1,7 +1,7 @@
 <template>
 <b-container>
   <b-row align-v="center" align-h="center">
-    <b-col align-self="baseline" class="mt-5 rounded-lg border shadow-sm p-4 mb-4 mr-1 bg-white">1 of 3</b-col>
+    <b-col align-self="baseline" class="mt-5 rounded-lg border shadow-sm p-4 mb-4 mr-1 bg-white"><resumecont-test></resumecont-test></b-col>
     <b-col cols="8" align-self="stretch" class="mt-5 rounded-lg border shadow-sm p-4 mb-4 ml-1 bg-white"><resumetemp-default></resumetemp-default></b-col>
   </b-row>
 </b-container>
@@ -9,13 +9,15 @@
 
 <script>
 import ResumeTemp_DefaultVue from '../components/ResumeTemp_Default.vue'
+import ResumeCont_TestVue from '../components/ResumeCont_Test.vue'
 // @ is an alias to /src
 
 
 export default {
   name: 'Resume_Builder',
   components: {
-    'resumetemp-default': ResumeTemp_DefaultVue
+    'resumetemp-default': ResumeTemp_DefaultVue,
+    'resumecont-test': ResumeCont_TestVue
   }
 }
 </script>
